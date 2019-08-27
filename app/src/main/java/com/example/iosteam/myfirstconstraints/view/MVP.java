1b878114646c6d4e5424bdd9b67a8570ce712524
jayababu <mariyarajendran.m@gmail.com>
2019-08-27 17:41:39 +0530

    Merge branch 'master' of /Users/iosteam/Desktop/Mariya with conflicts.

--- app/src/main/java/com/example/iosteam/myfirstconstraints/view/MVP.java
+++ app/src/main/java/com/example/iosteam/myfirstconstraints/view/MVP.java
@@ -22,8 +22,7 @@ public class MVP extends AppCompatActivity implements MVPVolleyContract.VolleyVi
     Intent mServiceIntent;
     String sample="";
     String manikandan="";
-    
-
+    String mariyaRajendran="";
 
 
     @Override
